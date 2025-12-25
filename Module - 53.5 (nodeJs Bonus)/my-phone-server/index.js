b560aee14d4cee39ee5fdb9086f634acf6215cbb
mujahidraj <mujahidraj65@gmail.com>
2025-12-25 16:57:40 +0600

    phone server

---
+++ Module - 53.5 (nodeJs Bonus)/my-phone-server/index.js
@@ -0,0 +1,23 @@
+const express= require('express')
+const phones= require('./phones.json')
+const app = express();
+const port = 3000;
+
+app.get('/' , (request , response)=>{
+  response.send("this is my phone server. data coming soon");
+})
+
+app.get('/phones',(request,response)=>{
+  response.send(phones)
+})
+
+app.get('/phones/:id' , (request , response)=>{
+  const id = parseInt(request.params.id);
+  console.log("I need data for :" ,id);
+const phoneData = phones.find(item=>item.id===id) || {};
+  response.send(phoneData)
+
+  
+})
+
+app.listen(port)
